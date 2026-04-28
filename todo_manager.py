ab69f53a8da923bd401e945670e3d4f0672ee75e
Danna Ruiz <dannaruji@unisabana.edu.co>
2026-04-27 21:24:00 -0500

    Lab8 2

--- todo_manager.py
+++ todo_manager.py
@@ -1,11 +1,20 @@
 """Laboratorio 8 - Módulo de persistencia para lista de tareas."""
 
-
 def read_todo_file(file_path):
     """Reads tasks from a file. Returns a list of tasks."""
-    # TODO: Implementar manejo de FileNotFoundError según README.md
-    raise NotImplementedError
+    try:
+        archivo = open(file_path, "r")
+        lista = []
 
+        for linea in archivo:
+            lista.append(linea.strip())
+
+        archivo.close()
+        return lista
+
+    except:
+        print(f"File {file_path} not found! Returning an empty to-do list.")
+        return []
 
 def write_todo_file(file_path, tasks):
     """Writes tasks to a file, one per line."""
